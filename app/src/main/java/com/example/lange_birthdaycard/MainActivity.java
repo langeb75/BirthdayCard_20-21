f76a0375e4f8127b107486f78e14237d7e78092b
Brad Lange <blange@gulllakecs.org>
2021-01-05 12:46:20 -0500

    Initial Birthday Card Commit

---
+++ app/src/main/java/com/example/lange_birthdaycard/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lange_birthdaycard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
